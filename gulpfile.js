8faea07ca8ef65fbad928a4c29815b230fb4b928
JastaFly <zumawuu@gmail.com>
2020-04-27 20:19:35 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+'use strict';
+const gulp = require('gulp');
+const {series, parallel, src, dest, watch} = require('gulp');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+const sass = require('gulp-sass');
+const prefixer = require('gulp-autoprefixer');
+const rigger = require('gulp-rigger');
+const rename = require("gulp-rename");
+
+
+function html() {
+    return src('./src/*.html')
+        .pipe(rigger())
+        .pipe(dest('./build/'))
+        .pipe(reload({stream: true}))
+}
+
+function scss() {
+    return src('./src/scss/main.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(prefixer({
+            cascade: false
+        }))
+        .pipe(dest('./build/css'))
+        .pipe(reload({stream: true}))
+}
+
+function img() {
+    return src('./src/img/**/*')
+        .pipe(dest('./build/img'))
+}
+
+function fonts() {
+    return src('./src/fonts/**/*')
+        .pipe(dest('./build/fonts'))
+}
+
+function libs() {
+    return src('./src/libs/**/*')
+        .pipe(dest('./build/libs'))
+}
+
+function js() {
+    return src('./src/js/**/*')
+        .pipe(dest('./build/js'))
+        .pipe(reload({stream: true}))
+}
+
+
+const config = {
+    server: {
+        baseDir: "./build"
+    },
+    tunnel: false,
+    host: 'localhost',
+    port: 9000,
+    logPrefix: "jf"
+};
+
+function webServer(cb) {
+    browserSync(config);
+    cb();
+}
+
+function watchAll(cb) {
+    watch('./src/*.html', html);
+    watch('./src/blocks/*.html', html);
+    watch('./src/**/*.scss', scss);
+    watch('./src/img/**/*', img);
+    watch('./src/fonts/**/*', fonts);
+    watch('./src/libs/**/*', libs);
+    watch('./src/js/**/*', js);
+    cb();
+}
+
+exports.default = parallel(webServer, watchAll);
---
+++ src/php/mail.php
@@ -0,0 +1,55 @@
+<?php
+if (!empty($_POST)) {
+    print_r($_POST);
+    // Файлы phpmailer
+    require '../libs/php-mailer/PHPMailer.php';
+    require '../libs/php-mailer/SMTP.php';
+    require '../libs/php-mailer/Exception.php';
+
+    if (isset($_POST['name']) && isset ($_POST['phone'])) {
+
+        $name = $_POST['name'];
+        $phone = $_POST['phone'];
+        $message = $_POST['message'];
+
+        $mail = new PHPMailer\PHPMailer\PHPMailer();
+        try {
+            $msg = "ok";
+            $mail->isSMTP();
+            $mail->CharSet = "UTF-8";
+            $mail->SMTPAuth = true;
+
+            // Настройки вашей почты
+            $mail->Host = 'smtp.yandex.ru';
+            $mail->Username = 'smtp_example@mail.ru';
+            $mail->Password = 'super_secret_password';
+            $mail->SMTPSecure = 'ssl';
+            $mail->Port = 465;
+            $mail->setFrom('example@mail.ru', 'Arthur Pupkin');
+
+            // Получатель письма
+            $mail->addAddress('recipient_example@mail.ru');
+
+            $mail->isHTML(true);
+
+            $mail->Subject = 'Новая заявка на звонок!';
+            $mail->Body = "<b>Имя:</b> $name <br>
+                <b>Тел:</b> $phone<br>
+                <b>Малява:</b> $message";
+
+            if ($mail->send()) {
+
+                echo "Ожидайте звонка, спасибо!";
+
+            } else {
+                echo "Сообщение не было отправлено. Неверно указаны настройки вашей почты";
+            }
+
+        } catch (Exception $e) {
+            echo "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+        }
+
+    } else {
+        echo "Сообщение не было отправлено. Пустые поля не допустимы!";
+    }
+}
