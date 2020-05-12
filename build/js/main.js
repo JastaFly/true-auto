c8b461ee1c98ca040f3974ad2594ca888bcb18c3
JastaFly <zumawuu@gmail.com>
2020-05-12 20:59:08 +0300

    desc complite

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,5 @@
+let count_item;
+
 $(document).ready(function () {
     $(".slider").owlCarousel({
         items: 1,
@@ -6,4 +8,23 @@ $(document).ready(function () {
         autoplay: true,
         autoplayTimeout: 25000
     });
+    $(".slider-two").owlCarousel({
+        items: 1,
+        nav: true,
+        dots: false,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 25000,
+    });
+
 });
+
+let slide_two = $('.slider-two');
+slide_two.on('changed.owl.carousel', function(event) {
+    let slide_count = document.getElementsByClassName('active')[2];
+    if (slide_count != undefined) {
+        var count_val = slide_count.children[0].getAttribute('count');
+        let count_elem = document.getElementsByClassName('slider-two__count')[0];
+        count_elem.innerHTML = '0' + count_val;
+    }
+})
