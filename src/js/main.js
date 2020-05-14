3567b8524187fc35b423dd12018ec8ae57b3c806
JastaFly <zumawuu@gmail.com>
2020-05-14 20:50:10 +0300

    Desk nearly ready

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,9 @@
-let count_item;
+let modal = document.getElementsByClassName('modal-window');
+let header = document.getElementsByTagName('header')[0];
+let main = document.getElementsByTagName('main')[0];
+let footer = document.getElementsByTagName('footer')[0];
+let shadow = document.getElementsByClassName('shadow')[0];
+let window_number;
 
 $(document).ready(function () {
     $(".slider").owlCarousel({
@@ -27,4 +32,21 @@ slide_two.on('changed.owl.carousel', function(event) {
         let count_elem = document.getElementsByClassName('slider-two__count')[0];
         count_elem.innerHTML = '0' + count_val;
     }
-})
+});
+
+const show_window = function (number) {
+    window_number = number;
+    $(modal[window_number]).slideToggle();
+    shadow.style.display = 'block';
+    header.style.filter = 'blur(10px)';
+    main.style.filter = 'blur(10px)';
+    footer.style.filter = 'blur(10px)';
+}
+
+const close_window = function () {
+    $(modal[window_number]).slideToggle();
+    shadow.style.display = 'none';
+    header.style.filter = 'blur(0)';
+    main.style.filter = 'blur(0)';
+    footer.style.filter = 'blur(0)';
+}
